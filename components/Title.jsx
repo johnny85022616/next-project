ca43f551e53e7ff9f21b565ae4aa9953e9c0a719
張裕 <nte.82002@yahoo.com.tw>
2021-10-09 16:50:43 +0800

    Title font-size 80px

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/system";
 
 export default function Title({children}) {
-    return <Box sx={{color:"primary.main" , fontSize:"40px" ,  fontWeight:"bold"}}>{children}</Box>
+    return <Box sx={{color:"primary.main" , fontSize:"80px" ,  fontWeight:"bold"}}>{children}</Box>
 };
